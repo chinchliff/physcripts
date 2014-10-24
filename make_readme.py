487b07bfd7acb8e36a4f8599ba8b2a2a56b96275
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-10-24 13:45:18 -0400

    editing docs

--- make_readme.py
+++ make_readme.py
@@ -23,7 +23,7 @@ The descriptions below include a variety of example calls to the scripts. The in
 
 ## Scripts
 
-What follows is a (complete) list of the available Python scripts, along with (highly incomplete) descriptive documentation about each one. More information can usually be found in the form of comments within the scripts (in addition, of course, to the hopefully somewhat self-documenting code itself).
+Below is a (complete) list of the available Python scripts, along with (highly incomplete) descriptive documentation about each one. Scripts with more complete documentation are listed before those without it. More information can usually be found in the form of comments within the scripts (in addition, of course, to my optimistically somewhat self-documenting code).
 
 """
 
